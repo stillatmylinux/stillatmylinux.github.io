7d816a2131dbbe6d9feb149e871ffa1fc8bfc71c
Matt Thiessen <m@mattt.us>
2020-04-04 13:29:55 -0500

    css nudge keys

---
+++ vuejs/2.0/keyboard/test.js
@@ -0,0 +1,7 @@
+if (this._currentLanguage === "en") {
+	this._currentLanguage = "ru";
+} else {
+	this._currentLanguage = "en";
+}
+
+this._currentLanguage = (this._currentLanguage === "en") ? 'ru' : 'en'
